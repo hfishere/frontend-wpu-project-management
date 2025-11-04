c683123f3d84f4cca02925d1467de97f0394c827
hfishere <hafidzfairiz@gmail.com>
2025-11-04 19:24:39 +0700

    Refactor import statement for 'path' in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import resolve from 'path';
+import { resolve } from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
 
 // https://vite.dev/config/
 export default defineConfig({
