396987112d8f757bd2619d9b4766bf7805d90f7a
hfishere <hafidzfairiz@gmail.com>
2025-11-04 17:21:34 +0700

    Implement SidebarLayout with Navbar and Sidebar components for improved navigation

---
+++ src/components/layouts/SidebarLayout/Sidebar/index.js
@@ -0,0 +1,5 @@
+import Sidebar from './Sidebar';
+import SidebarMenu from './SidebarMenu';
+
+export default Sidebar;
+export { SidebarMenu };
--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -1,34 +1,19 @@
-import { Box, Button } from '@mui/material';
-import { useState } from 'react';
+import { colors, Paper, Typography } from '@mui/material';
 
-import Modal from '../../ui/Modal';
+import SidebarLayout from '../../layouts/SidebarLayout';
 
 const Dashboard = () => {
-  const [openModal, setOpenModal] = useState();
-
-  const handleOpen = () => {
-    setOpenModal(true);
-  };
-  const handleClose = () => {
-    setOpenModal(false);
-  };
-
   return (
-    <Box>
-      <Button type="button" variant="contained" onClick={handleOpen}>
-        Open Modal
-      </Button>
-      <Modal open={openModal} handleClose={handleClose} title={'Judul Modal'}>
-        <Box
-          sx={{
-            padding: 2,
-            width: 500,
-          }}
-        >
-          Isi modal
-        </Box>
-      </Modal>
-    </Box>
+    <SidebarLayout pageTitle="Dashboard">
+      <Paper
+        sx={{
+          padding: 2,
+          background: colors.lightBlue[100],
+        }}
+      >
+        <Typography>Menampilkan dashboard di sini</Typography>
+      </Paper>
+    </SidebarLayout>
   );
 };
 
