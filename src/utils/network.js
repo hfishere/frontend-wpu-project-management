d322f42656d65f16aa988a63c5e9106f1f93af8f
hfishere <hafidzfairiz@gmail.com>
2025-11-05 13:04:25 +0700

    Refactor authentication logic to use updated API endpoints; enhance session management to store user data

--- src/utils/network.js
+++ src/utils/network.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import session from './session';
 
 const network = axios.create({
-  baseURL: 'http://127.0.0.1:3030/v1',
+  baseURL: 'http://127.0.0.1:3030/',
 });
 
 network.interceptors.request.use(
