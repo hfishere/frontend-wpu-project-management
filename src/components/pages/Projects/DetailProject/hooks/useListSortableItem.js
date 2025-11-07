937d8889bfa122d9f31fd0066747905fbc008f58
hfishere <hafidzfairiz@gmail.com>
2025-11-07 15:33:48 +0700

    Create Custom Hooks: useListSortableItem

---
+++ src/components/pages/Projects/DetailProject/hooks/useListSortableItem.js
@@ -0,0 +1,67 @@
+import { useDroppable } from '@dnd-kit/core';
+import { useSortable } from '@dnd-kit/sortable';
+import { useState } from 'react';
+
+import useDetailProjectContext from './useDetailProjectContext';
+
+import services from '@/services';
+import { DRAG_LIST } from '@/utils/constants';
+
+const useListSortableItem = ({ id, item }) => {
+  const detailProjectContext = useDetailProjectContext();
+  const taskItems = detailProjectContext.getTaskItemsByListId(item.public_id);
+
+  const [isShowConfirmDelete, setShowConfirmDelete] = useState(false);
+
+  const {
+    setNodeRef: setNodeRefDroppable,
+    isOver,
+    active,
+    over,
+  } = useDroppable({
+    id,
+    data: {
+      ...item,
+      type: DRAG_LIST,
+    },
+  });
+
+  const {
+    setNodeRef: setNodeRefSortable,
+    attributes,
+    listeners,
+    transform,
+    transition,
+  } = useSortable({
+    id,
+    data: {
+      ...item,
+      type: DRAG_LIST,
+    },
+  });
+
+  const handleDeleteList = (listId) => async () => {
+    await services.lists.remove(listId);
+    await detailProjectContext.fetchBoardLists();
+    setShowConfirmDelete(false);
+  };
+
+  return {
+    detailProjectContext,
+    taskItems,
+    isOver,
+    active,
+    over,
+    setNodeRefDroppable,
+    attributes,
+    listeners,
+    setNodeRefSortable,
+    transform,
+    transition,
+    handleDeleteList,
+    isShowConfirmDelete,
+    setShowConfirmDelete,
+  };
+};
+
+export default useListSortableItem;
